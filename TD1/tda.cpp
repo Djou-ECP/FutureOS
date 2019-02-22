d808ef53990cf7e45ac5add9c7e4652be1f85a83
Future <sebast.lubineau@laposte.net>
2019-02-22 09:32:31 -0500

    td1

--- TD1/tda.cpp
+++ TD1/tda.cpp
@@ -3,28 +3,27 @@
 
 using namespace std;
 
-void myHandler(int sig, siginfo_t* si, void*) {
-    if(si->si_value < 15) {
-        std::cout << "Compteur" << si->si_value << std::endl;
-        si->si_value++;
-    } else {
-        timer_delete(sig);
-    }
+void incrHandler(int sig, siginfo_t* si, void*) {
+    int* pcompteur = (int*) (si->si_value).sival_ptr;
+    cout << "Compteur: " << *pcompteur << endl;
+    *pcompteur += 1;
 }
 
 int main(int argc, char const *argv[]) {
+    int limit = 15;
+
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = myHandler;
+    sa.sa_sigaction = incrHandler;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGRTMIN, &sa, NULL);
 
     struct sigevent sev;
     sev.sigev_notify = SIGEV_SIGNAL;
     sev.sigev_signo = SIGRTMIN;
-    int compteur = 0;
+    int pcompteur = 0;
 
-    sev.sigev_value.sival_ptr = &compteur;
+    sev.sigev_value.sival_ptr = &pcompteur;
 
     timer_t tid;
     timer_create(CLOCK_REALTIME, &sev, &tid);
@@ -35,6 +34,11 @@ int main(int argc, char const *argv[]) {
     its.it_interval.tv_nsec = 500000000;
 
     timer_settime(tid, 0, &its, NULL);
+    while(pcompteur < limit ) {
+        // Wait for compteur to reach limit
+    }
+    timer_delete(tid);
+    cout << "Valeur finale: " << pcompteur << endl;
 
     return 0;
 }
