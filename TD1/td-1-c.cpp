6743f7578144dba406a44faffeefe893c3aede34
Djou <djou@via.ecp.fr>
2019-02-22 17:02:02 +0100

    Ajout exercice c

--- TD1/td-1-c.cpp
+++ TD1/td-1-c.cpp
@@ -9,30 +9,13 @@ void switchHandler(int sig, siginfo_t *si, void *) {
   *stop_ptr = true;
 }
 
-void startTimer(int sec, int nsec, volatile bool *pStop) {
-  struct sigaction sa;
-  sa.sa_flags = SA_SIGINFO;
-  sa.sa_sigaction = switchHandler;
-  sigemptyset(&sa.sa_mask);
-  sigaction(SIGRTMIN, &sa, NULL);
-
-  struct sigevent sev;
-  sev.sigev_notify = SIGEV_SIGNAL;
-  sev.sigev_signo = SIGRTMIN;
-
-  sev.sigev_value.sival_ptr = pStop;
-
-  timer_t tid;
-  timer_create(CLOCK_REALTIME, &sev, &tid);
+void startTimer(int sec, int nsec, volatile bool *pStop, timer_t *tid) {
   itimerspec its;
   its.it_value.tv_sec = sec;
   its.it_value.tv_nsec = nsec;
   its.it_interval.tv_sec = 0;
   its.it_interval.tv_nsec = 0;
-  timer_settime(tid, 0, &its, NULL);
-  while (!*pStop) {
-  }
-  timer_delete(tid);
+  timer_settime(*tid, 0, &its, NULL);
 }
 
 void incr(unsigned int nLoops, double *pCounter, volatile bool *pStop) {
@@ -42,19 +25,39 @@ void incr(unsigned int nLoops, double *pCounter, volatile bool *pStop) {
 }
 
 int main(int argc, char const *argv[]) {
+  volatile bool stop = false;
+
+  // Timer initialisation
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = switchHandler;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGRTMIN, &sa, NULL);
+
+  struct sigevent sev;
+  sev.sigev_notify = SIGEV_SIGNAL;
+  sev.sigev_signo = SIGRTMIN;
+  sev.sigev_value.sival_ptr = const_cast<bool *>(&stop);
+
+  timer_t tid;
+  timer_create(CLOCK_REALTIME, &sev, &tid);
+  // End timer initialisation
 
   unsigned int nLoops = UINT_MAX;
 
-  volatile bool stop = false;
   double counter = 0.0;
-  startTimer(1, 0, &stop);
-  timespec before = timespec_now();
+  std::cout << "Starting 1s timer..." << std::endl;
+  startTimer(1, 0, &stop, &tid);
   incr(nLoops, &counter, &stop);
-  timespec after = timespec_now();
-  timespec duration = after - before;
   std::cout << "Counter: " << counter << std::endl;
-  std::cout << "Execution lasted "
-            << (double)duration.tv_sec + (double)duration.tv_nsec * 1e-9
-            << " seconds." << std::endl;
+
+  stop = false;
+  counter = 0;
+
+  std::cout << "Starting 5s timer..." << std::endl;
+  startTimer(5, 0, &stop, &tid);
+  incr(nLoops, &counter, &stop);
+  std::cout << "Counter: " << counter << std::endl;
+
   return 0;
 }
