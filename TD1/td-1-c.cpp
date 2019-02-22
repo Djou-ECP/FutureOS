bf03078218f611144deaa5e28e40a33702fdb1d2
Djou <djou@via.ecp.fr>
2019-02-22 16:37:32 +0100

    Exercice c

---
+++ TD1/td-1-c.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+
+#include "timespec.h"
+
+void switchHandler(int sig, siginfo_t *si, void *) {
+  bool *stop_ptr = (bool *)(si->si_value).sival_ptr;
+  *stop_ptr = true;
+}
+
+void startTimer(int sec, int nsec, volatile bool *pStop) {
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = switchHandler;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGRTMIN, &sa, NULL);
+
+  struct sigevent sev;
+  sev.sigev_notify = SIGEV_SIGNAL;
+  sev.sigev_signo = SIGRTMIN;
+
+  sev.sigev_value.sival_ptr = pStop;
+
+  timer_t tid;
+  timer_create(CLOCK_REALTIME, &sev, &tid);
+  itimerspec its;
+  its.it_value.tv_sec = sec;
+  its.it_value.tv_nsec = nsec;
+  its.it_interval.tv_sec = 0;
+  its.it_interval.tv_nsec = 0;
+  timer_settime(tid, 0, &its, NULL);
+  while (!*pStop) {
+  }
+  timer_delete(tid);
+}
+
+void incr(unsigned int nLoops, double *pCounter, volatile bool *pStop) {
+  for (int i = 0; i < nLoops && !*pStop; i++) {
+    *pCounter += 1;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+
+  unsigned int nLoops = UINT_MAX;
+
+  volatile bool stop = false;
+  double counter = 0.0;
+  startTimer(1, 0, &stop);
+  timespec before = timespec_now();
+  incr(nLoops, &counter, &stop);
+  timespec after = timespec_now();
+  timespec duration = after - before;
+  std::cout << "Counter: " << counter << std::endl;
+  std::cout << "Execution lasted "
+            << (double)duration.tv_sec + (double)duration.tv_nsec * 1e-9
+            << " seconds." << std::endl;
+  return 0;
+}
